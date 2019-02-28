8afca3a6d1b331418c186a816bc574753fb2f755
ytppp <651003708@qq.com>
2019-02-28 22:47:31 +0800

    配置antd-design-vue按需加载

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { Button } from 'ant-design-vue'
+
+Vue.component(Button.name, Button)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a-button type="primary">Primary</a-button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue'
+
+export default {
+
+  components: {
+    AButton: Button
+  }
+}
+</script>
