90a5551c8a4a3dd58299d8a9adc58f98a0191d3d
ytppp <651003708@qq.com>
2019-03-06 00:24:32 +0800

    编写前台展示页面公共头部骨架

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button } from 'ant-design-vue'
-
-Vue.component(Button.name, Button)
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import '../src/assets/global.less'
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 new Vue({
   router,
